594b2295907baa5ace6810d52e34e3e32ce09b54
Volodymyr <tangrofromhabr@gmail.com>
2016-08-11 23:48:23 +0300

    stub to debug requests with Fiddler

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <QApplication>
 #include <QQmlContext>
 #include <QQmlApplicationEngine>
+ #include <QNetworkProxy>
 
 #include "settings.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    //QNetworkProxy proxy(QNetworkProxy::HttpProxy, "127.0.0.1", 8888);
+    //QNetworkProxy::setApplicationProxy(proxy);
 
     QQmlApplicationEngine engine;
     Settings* settings = new Settings();
